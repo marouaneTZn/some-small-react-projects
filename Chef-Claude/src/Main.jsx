e63c870e1beaec0f382e8758e2d27fb90470dc10
marouaneTZn <marouane1touzani@gmail.com>
2026-02-16 01:31:36 +0000

    I add the FormData is a recommended method in React is to use controlled components, where form data is managed directly within the component's state.

--- Chef-Claude/src/Main.jsx
+++ Chef-Claude/src/Main.jsx
@@ -1,22 +1,20 @@
-import react from "react";
+import React from "react";
 
 export default function Main() {
-  const [ingredients, setIngredients] = react.useState([]);
+  const [ingredients, setIngredients] = React.useState([]);
 
   const ingredientsListItems = ingredients.map((ingredient) => (
     <li key={ingredient}>{ingredient}</li>
   ));
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
+  function addIngredient(formData) {
     const newIngredient = formData.get("ingredient");
-    setIngredients((hello) => [...hello, newIngredient]);
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
   }
 
   return (
     <main>
-      <form onSubmit={handleSubmit} className="add-ingredient-form">
+      <form action={addIngredient} className="add-ingredient-form">
         <input
           type="text"
           placeholder="e.g. oregano"
